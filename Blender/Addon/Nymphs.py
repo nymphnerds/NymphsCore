d09dca96dde80f28031b6a7a1a1d3d903bc96417
Babyjawz <nymph@BabyJ-H2O.localdomain>
2026-04-24 11:17:57 +0100

    Reload addon implementation on extension update

--- Blender/Addon/Nymphs.py
+++ Blender/Addon/Nymphs.py
@@ -5,7 +5,7 @@ Live Blender addon implementation for Nymphs.
 bl_info = {
     "name": "Nymphs",
     "author": "Nymphs3D",
-    "version": (1, 1, 168),
+    "version": (1, 1, 169),
     "blender": (4, 2, 0),
     "location": "View3D > Sidebar > Nymphs",
     "description": "Blender client for NymphsCore image, shape, and texture backends",
--- Blender/Addon/__init__.py
+++ Blender/Addon/__init__.py
@@ -1,6 +1,14 @@
 """Live Blender addon entrypoint for Nymphs."""
 
-try:
-    from .Nymphs import *  # noqa: F401,F403
-except ImportError:
-    from Nymphs import *  # type: ignore # noqa: F401,F403
+import importlib
+
+
+_module_name = f"{__package__}.Nymphs" if __package__ else "Nymphs"
+_module = importlib.import_module(_module_name)
+_module = importlib.reload(_module)
+
+_public_names = getattr(_module, "__all__", None)
+if _public_names is None:
+    _public_names = [name for name in dir(_module) if not name.startswith("_")]
+
+globals().update({name: getattr(_module, name) for name in _public_names})
