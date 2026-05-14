7eeb273b3be549b4bfe9247ed0f852d49007ba1a
NymphsCore Maintainers <nymphnerds@users.noreply.github.com>
2026-05-14 21:07:12 +0100

    Align compact secret action rows

--- Manager/apps/NymphsCoreManager/Models/NymphModuleActionGroupInfo.cs
+++ Manager/apps/NymphsCoreManager/Models/NymphModuleActionGroupInfo.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using NymphsCoreManager.ViewModels;
 
 namespace NymphsCoreManager.Models;
@@ -63,7 +64,13 @@ public sealed class NymphModuleActionGroupInfo
 
     public bool HasNoOptionFields => !HasOptionFields;
 
-    public int FieldLabelWidth => HasOptionFields ? 168 : 145;
+    public int FieldRowLeftMargin => HasOptionFields ? 24 : 0;
+
+    public Thickness FieldRowMargin => new(FieldRowLeftMargin, 0, 0, 6);
+
+    public int FieldLabelWidth => HasOptionFields ? 168 : 128;
+
+    public string FieldLabelAlignment => HasOptionFields ? "Right" : "Left";
 
     public int FieldControlWidth => 220;
 
